9e9d76d09fb2539feae2301031b418c7348eb3d0
Nate Paradiso <ipfcolo05@gmail.com>
2024-03-12 21:55:46 -0700

    setting up paypal

--- server.js
+++ server.js
@@ -1,167 +1,160 @@
 import express from "express";
+import fetch from "node-fetch";
 import cors from "cors";
 import "dotenv/config";
 
-// const app = express();
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   }),
-// );
-// app.use(express.static("./public"));
-// app.use(express.json());
+import path from "path";
 
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//   console.log(`running at http://localhost:${PORT}`);
-// });
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
+const base = "https://api-m.sandbox.paypal.com";
+const app = express();
 
-// import express from "express";
-// import fetch from "node-fetch";
-// import "dotenv/config";
-// import path from "path";
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Replace with the origin you want to allow
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+// host static files
+app.use(express.static("client"));
 
-// const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
-// const base = "https://api-m.sandbox.paypal.com";
-// const app = express();
+// parse post params sent in body in json format
+app.use(express.json());
 
-// // host static files
-// app.use(express.static("client"));
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Server is RAD!" });
+});
 
-// // parse post params sent in body in json format
-// app.use(express.json());
+/**
+ * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
+ * @see https://developer.paypal.com/api/rest/authentication/
+ */
+const generateAccessToken = async () => {
+  try {
+    if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+      throw new Error("MISSING_API_CREDENTIALS");
+    }
+    const auth = Buffer.from(PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "POST",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
 
-// /**
-//  * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
-//  * @see https://developer.paypal.com/api/rest/authentication/
-//  */
-// const generateAccessToken = async () => {
-//   try {
-//     if (!PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
-//       throw new Error("MISSING_API_CREDENTIALS");
-//     }
-//     const auth = Buffer.from(PAYPAL_CLIENT_ID + ":" + PAYPAL_CLIENT_SECRET).toString("base64");
-//     const response = await fetch(`${base}/v1/oauth2/token`, {
-//       method: "POST",
-//       body: "grant_type=client_credentials",
-//       headers: {
-//         Authorization: `Basic ${auth}`,
-//       },
-//     });
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Failed to generate Access Token:", error);
+  }
+};
 
-//     const data = await response.json();
-//     return data.access_token;
-//   } catch (error) {
-//     console.error("Failed to generate Access Token:", error);
-//   }
-// };
+/**
+ * Create an order to start the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
+ */
+const createOrder = async cart => {
+  // use the cart information passed from the front-end to calculate the purchase unit details
+  console.log("shopping cart information passed from the frontend createOrder() callback:", cart);
 
-// /**
-//  * Create an order to start the transaction.
-//  * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
-//  */
-// const createOrder = async cart => {
-//   // use the cart information passed from the front-end to calculate the purchase unit details
-//   console.log("shopping cart information passed from the frontend createOrder() callback:", cart);
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders`;
+  const payload = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: "100.00",
+        },
+      },
+    ],
+  };
 
-//   const accessToken = await generateAccessToken();
-//   const url = `${base}/v2/checkout/orders`;
-//   const payload = {
-//     intent: "CAPTURE",
-//     purchase_units: [
-//       {
-//         amount: {
-//           currency_code: "USD",
-//           value: "100.00",
-//         },
-//       },
-//     ],
-//   };
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
 
-//   const response = await fetch(url, {
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${accessToken}`,
-//       // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
-//       // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-//       // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
-//       // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
-//       // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
-//     },
-//     method: "POST",
-//     body: JSON.stringify(payload),
-//   });
+  return handleResponse(response);
+};
 
-//   return handleResponse(response);
-// };
+/**
+ * Capture payment for the created order to complete the transaction.
+ * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
+ */
+const captureOrder = async orderID => {
+  const accessToken = await generateAccessToken();
+  const url = `${base}/v2/checkout/orders/${orderID}/capture`;
 
-// /**
-//  * Capture payment for the created order to complete the transaction.
-//  * @see https://developer.paypal.com/docs/api/orders/v2/#orders_capture
-//  */
-// const captureOrder = async orderID => {
-//   const accessToken = await generateAccessToken();
-//   const url = `${base}/v2/checkout/orders/${orderID}/capture`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+      // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
+      // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
+    },
+  });
 
-//   const response = await fetch(url, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${accessToken}`,
-//       // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
-//       // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-//       // "PayPal-Mock-Response": '{"mock_application_codes": "INSTRUMENT_DECLINED"}'
-//       // "PayPal-Mock-Response": '{"mock_application_codes": "TRANSACTION_REFUSED"}'
-//       // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
-//     },
-//   });
+  return handleResponse(response);
+};
 
-//   return handleResponse(response);
-// };
+async function handleResponse(response) {
+  try {
+    const jsonResponse = await response.json();
+    return {
+      jsonResponse,
+      httpStatusCode: response.status,
+    };
+  } catch (err) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
 
-// async function handleResponse(response) {
-//   try {
-//     const jsonResponse = await response.json();
-//     return {
-//       jsonResponse,
-//       httpStatusCode: response.status,
-//     };
-//   } catch (err) {
-//     const errorMessage = await response.text();
-//     throw new Error(errorMessage);
-//   }
-// }
+app.post("/api/orders", async (req, res) => {
+  try {
+    // use the cart information passed from the front-end to calculate the order amount detals
+    const { cart } = req.body;
+    const { jsonResponse, httpStatusCode } = await createOrder(cart);
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to create order." });
+  }
+});
 
-// app.post("/api/orders", async (req, res) => {
-//   try {
-//     // use the cart information passed from the front-end to calculate the order amount detals
-//     const { cart } = req.body;
-//     const { jsonResponse, httpStatusCode } = await createOrder(cart);
-//     res.status(httpStatusCode).json(jsonResponse);
-//   } catch (error) {
-//     console.error("Failed to create order:", error);
-//     res.status(500).json({ error: "Failed to create order." });
-//   }
-// });
+app.post("/api/orders/:orderID/capture", async (req, res) => {
+  try {
+    const { orderID } = req.params;
+    const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error("Failed to create order:", error);
+    res.status(500).json({ error: "Failed to capture order." });
+  }
+});
 
-// app.post("/api/orders/:orderID/capture", async (req, res) => {
-//   try {
-//     const { orderID } = req.params;
-//     const { jsonResponse, httpStatusCode } = await captureOrder(orderID);
-//     res.status(httpStatusCode).json(jsonResponse);
-//   } catch (error) {
-//     console.error("Failed to create order:", error);
-//     res.status(500).json({ error: "Failed to capture order." });
-//   }
-// });
+// serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./client/checkout.html"));
+});
 
-// // serve index.html
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve("./client/checkout.html"));
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Node server listening at http://localhost:${PORT}/`);
-// });
+app.listen(PORT, () => {
+  console.log(`Node server listening at http://localhost:${PORT}/`);
+});
