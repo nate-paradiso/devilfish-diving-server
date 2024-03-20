810a5f71cb0b779d7e62cc7b0e63351566a9f7ac
Nate Paradiso <ipfcolo05@gmail.com>
2024-03-20 11:50:29 -0700

    payapl sdk sandbox environment

---
+++ README.md
@@ -0,0 +1,47 @@
+Paypal SDK Transaction Server Environment
+
+This is the backend server for Devilfish Diving, a dive charter website. It provides endpoints for handling PayPal transactions and integrates with the Devilfish Diving client.
+Here is the client repo. https://github.com/nate-paradiso/devilfish-diving-client
+
+Getting Started
+To get started with the Devilfish Diving server, follow these steps:
+
+Installation
+Clone this repository to your local machine:
+
+zsh
+Copy code
+git clone https://github.com/nate-paradiso/devilfish-diving-server.git
+Navigate into the project directory:
+
+zsh
+Copy code
+cd devilfish-diving-server
+Install dependencies using npm:
+
+zsh
+Copy code
+npm install
+Configuration
+Before running the server, you'll need to set up your environment variables. Create a .env file in the root directory of the project and add the following variables:
+
+PAYPAL_CLIENT_ID: Your PayPal client ID.
+PAYPAL_CLIENT_SECRET: Your PayPal client secret.
+SANDBOX_MODE: Set this to true if you want to use the PayPal sandbox environment for testing.
+Running the Server
+Start the server using the following command:
+
+zsh
+Copy code
+npm start
+The server will be accessible at http://localhost:8888.
+
+Testing PayPal Transactions
+Important: This server is configured to work with the PayPal sandbox environment for testing purposes. Make sure to set SANDBOX_MODE to true in your .env file to use the sandbox environment.
+
+Usage
+Once the server is running, it will handle incoming requests from the Devilfish Diving client to initiate and capture PayPal transactions. Make sure the client is configured to send requests to the correct endpoint.
+
+Contributing
+Contributions to the Devilfish Diving server are welcome! If you'd like to contribute, please follow these guidelines:
+
--- server.js
+++ server.js
@@ -3,28 +3,20 @@ import fetch from "node-fetch";
 import cors from "cors";
 import "dotenv/config";
 
-import path from "path";
-
 const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, PORT = 8888 } = process.env;
 const base = "https://api-m.sandbox.paypal.com";
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // Replace with the origin you want to allow
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-// host static files
-app.use(express.static("client"));
 
 // parse post params sent in body in json format
 app.use(express.json());
-
-app.get("/api/test", (req, res) => {
-  res.json({ message: "Server is RAD!" });
-});
 
 /**
  * Generate an OAuth 2.0 access token for authenticating with PayPal REST APIs.
@@ -55,31 +47,28 @@ const generateAccessToken = async () => {
  * Create an order to start the transaction.
  * @see https://developer.paypal.com/docs/api/orders/v2/#orders_create
  */
-const createOrder = async cart => {
-  // use the cart information passed from the front-end to calculate the purchase unit details
-  console.log("shopping cart information passed from the frontend createOrder() callback:", cart);
-
+const createOrder = async trip => {
   const accessToken = await generateAccessToken();
   const url = `${base}/v2/checkout/orders`;
   const payload = {
     intent: "CAPTURE",
     purchase_units: [
       {
+        reference_id: "2 tank dive trip", // Make sure this matches your item ID
         amount: {
           currency_code: "USD",
-          value: "100.00",
+          value: trip.cost, // Update this to the actual cost of the item
         },
       },
     ],
   };
-
   const response = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${accessToken}`,
       // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
       // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}',
       // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
       // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
     },
@@ -129,9 +118,8 @@ async function handleResponse(response) {
 
 app.post("/api/orders", async (req, res) => {
   try {
-    // use the cart information passed from the front-end to calculate the order amount detals
-    const { cart } = req.body;
-    const { jsonResponse, httpStatusCode } = await createOrder(cart);
+    const { trip } = req.body;
+    const { jsonResponse, httpStatusCode } = await createOrder(trip);
     res.status(httpStatusCode).json(jsonResponse);
   } catch (error) {
     console.error("Failed to create order:", error);
@@ -148,11 +136,6 @@ app.post("/api/orders/:orderID/capture", async (req, res) => {
     console.error("Failed to create order:", error);
     res.status(500).json({ error: "Failed to capture order." });
   }
-});
-
-// serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./client/checkout.html"));
 });
 
 app.listen(PORT, () => {
